42988dc2f797d9fb457ddd24556497a4357c267c
David <david_ristov@live.com>
2022-05-16 21:28:06 +0200

    change tag with yq

--- .github/script.sh
+++ .github/script.sh
@@ -5,8 +5,7 @@ git config --global user.name "argocd-bot"
 
 git clone https://$PAT@github.com/davidristov/argocd-config.git
 cd argocd-config 
-export tag=`yq e '.global.image.tag' values.yaml`
-sed -i "s/$tag/$GITHUB_SHA/g" values.yaml
+yq eval ".global.image.tag = \"$GITHUB_SHA\"" -i values.yaml
 
 git add values.yaml
 git commit -m "bump image version"
